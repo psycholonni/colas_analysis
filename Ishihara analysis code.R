90cc2f8e10d506418e7290760092de1e459cd775
psycholonni <lmar0020@student.monash.edu>
2022-07-06 17:26:11 +1000

    Ishihara/survey script tidy-up
    Add mean rt to Ishihara df
    Tidy calculations of score
    plot of distribution

--- Ishihara analysis code.R
+++ Ishihara analysis code.R
@@ -16,7 +16,7 @@ setwd("~/thesis/colas_analysis")
 
 #MAKE DATAFRAME WITH ONE ROW PER ANSWER
 {
-IH_vars<- c("participant", "Ishi_textbox.text", "imageaddress")
+IH_vars<- c("participant", "Ishi_textbox.text", "Ishi_trial_keyresp.rt", "imageaddress")
 
 #make catch data frame 
 IHdata<-data[IH_vars]
@@ -30,18 +30,21 @@ scoringtable<- read_excel("PictureConditions.xlsx")
 
 #inserting correct answers to participants dataframe
 IHdata <- merge(IHdata, scoringtable, by= "imageaddress")
-IHdata$match <- IHdata$Ishi_textbox.text==IHdata$correctanswer
+
+#coercing answers into numeric for efficient matching
+IHdata$answer.numeric <- as.numeric(IHdata$Ishi_textbox.text)
+IHdata$match <- IHdata$answer.numeric==IHdata$correctanswer
 
 
 #MAKE IH_PERPERSON DATAFRAME WITH ONE ROW PER PARTICIPANT
 {
-
 #make dataframe with IH scores
-IH_perperson<- data.frame(unique(participants_complete$participant))
-IH_perperson<-rename(IH_perperson, participant = unique.participants_complete.participant.)
+IH_perperson<- data.frame(unique(IHdata$participant))
+IH_perperson<-rename(IH_perperson, participant = unique.IHdata.participant.)
+}
 
 #get IH score as decimal, not sure that's useful
-#z=participant id 
+{#z=participant id 
 get_IHscoredecimal<- function(z){ 
   temp_df <- subset(IHdata,participant==z)
   total_IH <- nrow(temp_df)
@@ -50,10 +53,10 @@ get_IHscoredecimal<- function(z){
   IHscoredecimal<-as.numeric(IHscoredecimal)
   return(IHscoredecimal)
 }
-#IH_perperson$score_decimal<-lapply(IH_perperson$participant, get_IHscoredecimal)  
-
+IH_perperson$score_decimal<-lapply(IH_perperson$participant, get_IHscoredecimal)  
+}
 #get IH score as fraction
-#z=participant id
+{#z=participant id
 get_IHscorefraction<- function(z){ 
   temp_df <- subset(IHdata,participant==z)
   total_IH <- nrow(temp_df)
@@ -63,27 +66,47 @@ get_IHscorefraction<- function(z){
 }
 IH_perperson$score_fraction<-lapply(IH_perperson$participant, get_IHscorefraction)
 }
-
-get_IHtotal_answered<- function(z){ 
+#get IH correctly answered 
+{get_IHcorrectQs<- function(z){ 
+  temp_df <- subset(IHdata,participant==z)
+  correct_IH<- nrow(subset(temp_df,match==TRUE))
+  return(correct_IH)
+}
+  IH_perperson$correctA<-lapply(IH_perperson$participant, get_IHcorrectQs)
+  IH_perperson$correctA <- as.numeric(IH_perperson$correctA)
+}
+#get IH questions answered 
+{get_IHtotal_answered<- function(z){ 
   temp_df <- subset(IHdata,participant==z)
   total_IH <- nrow(temp_df)
    return(total_IH)
 }
 IH_perperson$total_answered<-lapply(IH_perperson$participant, get_IHtotal_answered)
 IH_perperson$total_answered <- as.numeric(IH_perperson$total_answered)
+}
+#get average response time
+{get_IHrt<- function(z){
+  temp_df <- subset(IHdata,participant==z)
+  rt_IH<-mean(temp_df$Ishi_trial_keyresp.rt)
+  return(rt_IH)
+}
+IH_perperson$mean.rt <- lapply(IH_perperson$participant, get_IHrt)
+IH_perperson$mean.rt <- as.numeric((IH_perperson$mean.rt))
+}
 
+#subsetting a single participant
+singleIHdata<-IHdata[IHdata$participant == "61716a16c157db249e36fc46", ]
+head(singleIHdata)
 
-#Rearrange dataframe 
-#IH_perperson$score_decimal <- as.numeric(IH_perperson$score_decimal)
-#IH_ordered <- IH_perperson[order(IH_perperson$score_decimal),]
-IH_ordered <- IH_perperson[order(IH_perperson$total_answered),]
+#this participant submitted two files
+IH_perperson <- subset(IH_perperson,!participant == "61716a16c157db249e36fc46")
 
-IH_perperson$score <- as.numeric(IH_perperson$score_fraction)
-#Need to make a unique score to filter answers <14 & >16 
-
-IH_perperson$decimal <- sapply(IH_perperson$score_fraction, function(x) eval(parse(text=x)))
-
-#single participants
-singleIHdata<-IHdata[IHdata$participant == "5ec3eefc909cda09ed268192", ]
-singleIHdata
-surveydata<-data[, survey_vars]
+#Distribution of IH scores 
+ggplot(IH_perperson, aes(correctA)) +
+  geom_histogram() +
+  scale_x_continuous(breaks = as.numeric(round(quantile(IH_perperson$correctA),digits = 2))) + 
+  theme_pubr() +   
+  theme(axis.line = element_blank())+
+  geom_rangeframe()+
+  labs(x="Ishihara score",
+       title= "Histogram of Ishihara score")
--- Survey .R
+++ Survey .R
@@ -1,10 +1,20 @@
 #Survey dataframe 
 #select catch variables for analysis
 survey_vars<- c("participant", "textbox.text", "textbox_2.text", "textbox_3.text", "textbox_4.text", "textbox_5.text", "textbox_6.text")
+#, "textbox_7.text")
 
-, "textbox_7.text"
 #make catch data frame 
-surveydata<-data[data$participant == "61652222bfd9351c91a6585a", survey_vars]
 surveydata<-data[, survey_vars]
+#surveydata<-data[data$participant == "61652222bfd9351c91a6585a", survey_vars]
+
+#rename answers to make some sense
+surveydata <- surveydata %>% rename(location.diffi=textbox.text,
+                                    colour.diff= textbox_2.text,
+                                    normal.acuity= textbox_3.text,
+                                    colour.vision= textbox_4.text,
+                                    answer.strategy = textbox_5.text,
+                                    serious.check = textbox_6.text)
 #remove empty cells
 surveydata <- surveydata %>% drop_na()
+
+#could try to do a word cloud or something but would need to reformat data so that y/Y/Yes/yes
